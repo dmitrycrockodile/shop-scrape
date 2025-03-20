16ccc8c53698c06f4e46d0e01d92cf71a37b4bc0
Dmitrij <dmitry.bermas@gmail.com>
2025-03-20 11:48:10 +0100

    fix: deleted registration

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,5 @@ Route::middleware('auth:sanctum')->group(function() {
 
 Route::middleware('guest:sanctum')->group(function() {
    // Auth related, accessible for guests routes
-   Route::post('/register', [RegisterController::class, 'register']);
    Route::post('/login', [LoginController::class, 'login']);
 });
