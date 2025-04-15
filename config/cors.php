e9b24231de8b56d931c8b96bbdb7a5147541c5d9
Dmitrij <dmitry.bermas@gmail.com>
2025-04-15 17:06:20 +0200

    fix(cors.php): added deployed ui path

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:8080')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:8080'), 'https://bermas.dmytro.ui.quest.groupbwt.dev'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
